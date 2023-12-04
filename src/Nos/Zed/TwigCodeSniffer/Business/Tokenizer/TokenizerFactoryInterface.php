bee7987168c370936d4eb34326bd8f7acda44584
Andreas Penz <andreas.penz.1989@gmail.com>
2023-12-04 13:57:22 +0100

    add license information

--- src/Nos/Zed/TwigCodeSniffer/Business/Tokenizer/TokenizerFactoryInterface.php
+++ src/Nos/Zed/TwigCodeSniffer/Business/Tokenizer/TokenizerFactoryInterface.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Copyright (c) Andreas Penz
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 declare(strict_types=1);
 
 namespace Nos\Zed\TwigCodeSniffer\Business\Tokenizer;
